9ccb5ce11ee9453b6cdbbb548afafa93d3c81597
Дмитрий Смирнов <dsmirnov>
2018-02-16 10:37:24 +0000

    added missing classes

---
+++ src/uk/ac/ebi/ena/sra/pipeline/resource/ProcessResourceLock.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.ena.sra.pipeline.resource;
+
+public class 
+ProcessResourceLock extends ResourceLock
+{
+
+	public
+	ProcessResourceLock( String lock_id, String lock_owner ) 
+	{
+		super(lock_id, lock_owner);
+	}
+}
---
+++ src/uk/ac/ebi/ena/sra/pipeline/resource/StageResourceLock.java
@@ -0,0 +1,11 @@
+package uk.ac.ebi.ena.sra.pipeline.resource;
+
+public class 
+StageResourceLock extends ResourceLock 
+{
+	public 
+	StageResourceLock( String lock_id, String lock_owner )
+	{
+		super( lock_id, lock_owner );
+	}
+}
