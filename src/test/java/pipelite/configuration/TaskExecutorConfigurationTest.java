da932588aff16aaa096fe96246c2930501adc501
rasko <rasko@ebi.ac.uk>
2020-08-16 17:35:51 +0100

    Code improvements.

--- src/test/java/pipelite/configuration/TaskExecutorConfigurationTest.java
+++ src/test/java/pipelite/configuration/TaskExecutorConfigurationTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import uk.ac.ebi.ena.sra.pipeline.launcher.Launcher;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = Launcher.class)
+@ActiveProfiles("test")
 @EnableConfigurationProperties(value = TaskExecutorConfiguration.class)
 public class TaskExecutorConfigurationTest {
 
